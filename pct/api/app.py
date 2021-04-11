0ac3e479dd72ec035776a0dba046c079ec7dcbc9
Yevgeniy Yermoshin <yev.developer@gmail.com>
2021-04-12 00:55:27 +0100

    Updating API for optimizer Restful end-point

--- pct/api/app.py
+++ pct/api/app.py
@@ -1,12 +1,20 @@
+import os
 import logging
 import sys
 import connexion
 
 from connexion.resolver import RestyResolver
 
+from pct.data.data_reader import SimpleIndexedCSVDataset
+from pct.core.parameters import OptimizerParameters
+from pct.core.optimizer import SimplePortfolioReturnOptimizer
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s | %(levelname)s : %(message)s', level=logging.INFO, stream=sys.stdout)
 
+DATA_DIR = os.getenv('DATA_DIR', os.path.join(os.path.expanduser('~'), r'Data/quant/prices'))
+HISTORICAL_PRICES_5_YRS = os.path.join(DATA_DIR, 'prices_5yrs.csv')
+HISTORICAL_PRICES_10_YRS = os.path.join(DATA_DIR, 'prices_10yrs.csv')
 
 def status():
     return 'OK', 200
@@ -17,8 +25,34 @@ def tag():
 def metadata():
     return {"name" : tag()}
 
-def optimize(**kwargs):
-    return {}
+def optimize(body):
+
+    # Should come from service and persistent storage
+    pricing_service = SimpleIndexedCSVDataset(fpath=HISTORICAL_PRICES_5_YRS)
+    df_prices = pricing_service.read()
+
+
+    # Should come from instruments service with validation
+    df_portfolio = df_prices[df_prices['ticker'].isin(['AAPL','MSFT','AMZN'])]
+
+    tickers = ['AAPL','MSFT','AMZN']
+    constraints = {'sum_of_weights' : 'eq'}
+
+    request = {}
+    request[OptimizerParameters.TAG_KEY] = body.get(OptimizerParameters.TAG_KEY)
+    request[OptimizerParameters.TICKERS_KEY] = body.get(OptimizerParameters.TICKERS_KEY)
+    request[OptimizerParameters.FUNCTION_KEY] = body.get(OptimizerParameters.FUNCTION_KEY)
+
+    request[OptimizerParameters.CONSTRAINTS_KEY] = constraints
+
+    parameters = OptimizerParameters(**request)
+
+    optimizer_service = SimplePortfolioReturnOptimizer(parameters)
+
+    result = optimizer_service.optimize(df_portfolio=df_portfolio)
+
+
+    return result
 
 
 
